35c4074df7ac9ac628005485bb0ea56e34666748
kemot1709 <kemot1709@gmail.com>
2019-03-31 16:37:12 +0200

    Remake program, delete unimportand trash, improve code transparency

--- lab1/script/zolw.py
+++ lab1/script/zolw.py
@@ -1,46 +1,33 @@
 #!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
-from geometry_msgs.msg import Twist
-import sys, termios, tty, os, time
 
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-button_delay = 0.2
+import click
+import rospy
+from geometry_msgs.msg import Twist
+
 
 def zolw():
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('zolw', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rospy.init_node('zolw')
     
     while not rospy.is_shutdown():
-        key = getch()
+        twist = Twist()
+	key = click.getchar()
+
         up = rospy.get_param("up")
         down = rospy.get_param("down")
         left = rospy.get_param("left")
         right = rospy.get_param("right")
-        msg_t = Twist()
+
         if key == up :
-            msg_t.linear.x=2.0
+            twist.linear.x=2.0
         if key == down :
-            msg_t.linear.x=-2.0
+            twist.linear.x=-1.0
         if key == left :
-            msg_t.angular.z=1.8
+            twist.angular.z=1.5
         if key == right :
-            msg_t.angular.z=-1.8
-        rospy.loginfo(msg_t)
-        rospy.loginfo(key)
-        pub.publish(msg_t)
-        if key == 'c' :
-            sys.exit(0)
-        rate.sleep()
+            twist.angular.z=-1.5
+
+        pub.publish(twist)
 
 if __name__ == '__main__':
     try:
