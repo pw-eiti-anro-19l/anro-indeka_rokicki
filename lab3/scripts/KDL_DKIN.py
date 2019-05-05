b9017d48d99ab0bac3e3d1bcfda5cf3114c55ca9
kemot1709 <kemot1709@gmail.com>
2019-05-06 00:45:26 +0200

    improved kdl, parameters included

--- lab3/scripts/KDL_DKIN.py
+++ lab3/scripts/KDL_DKIN.py
@@ -4,6 +4,7 @@ import rospy
 import PyKDL as kdl
 import json
 import os
+import math as m
 from geometry_msgs.msg import PoseStamped #Publish Header header Pose pose
 from sensor_msgs.msg import JointState #Subscribe Header header string[] name float64[] position float64[] velocity float64[] effort
 #Pose Point position Quaternion orientation
@@ -20,25 +21,16 @@ def my_node():
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + 'I heard position %f from %s', data.position[0], data.name[0])
 
-    chain = kdl.Chain()
-#   helpFrame = kdl.Frame()
-#
-#   outFrame = helpFrame.DH(0, 0, 0, 0)
-#   chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.None), outFrame))
-#   outFrame = helpFrame.DH(a1, d1, al1, th1)
-#   chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.TransZ), outFrame))
-#   outFrame = helpFrame.DH(a2, d2, al2, th2)
-#   chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.TransZ), outFrame))
-#   outFrame = helpFrame.DH(a3, d3, al3, th3)
-#   chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.TransZ), outFrame))
+    chain = kdl.Chain()    
 
-    rot = kdl.Rotation(0,1,0, 0,0,1, 1,0,0)
-    
-    #chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.None), kdl.Frame(kdl.Vector(0.0,0.0,0.0))))
-    chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.TransZ), kdl.Frame(rot, kdl.Vector(1.0,0.0,0.0))))
-    chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.TransZ), kdl.Frame(rot, kdl.Vector(1.0,0.0,0.0))))
-    chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.TransZ), kdl.Frame(rot, kdl.Vector(1.0,0.0,0.0))))
-
+    rot = kdl.Rotation(m.cos(th3), m.sin(th3), 0, -1*m.sin(th3)*m.cos(al3), m.cos(th3)*m.cos(al3), m.sin(al3), m.sin(th3)*m.sin(al3), -1*m.cos(th3)*m.sin(al3), m.cos(al3))
+    chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.TransZ), kdl.Frame(rot, kdl.Vector(a3,0.0,d3))))
+    rot = kdl.Rotation(m.cos(th2), m.sin(th2), 0, -1*m.sin(th2)*m.cos(al2), m.cos(th2)*m.cos(al2), m.sin(al2), m.sin(th2)*m.sin(al2), -1*m.cos(th2)*m.sin(al2), m.cos(al2))
+    chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.TransZ), kdl.Frame(rot, kdl.Vector(a2,0.0,d2))))
+    rot = kdl.Rotation(m.cos(th1), m.sin(th1), 0, -1*m.sin(th1)*m.cos(al1), m.cos(th1)*m.cos(al1), m.sin(al1), m.sin(th1)*m.sin(al1), -1*m.cos(th1)*m.sin(al1), m.cos(al1))
+    chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.TransZ), kdl.Frame(rot, kdl.Vector(a1,0.0,d1))))
+    rot = kdl.Rotation(1,0,0, 0,1,0, 0,0,1)
+    chain.addSegment(kdl.Segment(kdl.Joint(kdl.Joint.None), kdl.Frame(rot, kdl.Vector(0.0,0.0,0.0))))
 
     fksolver = kdl.ChainFkSolverPos_recursive(chain)
 
@@ -54,7 +46,7 @@ def callback(data):
     pub_msg = PoseStamped()
     pub_msg.header.frame_id = "base_link"
     pub_msg.pose.position.x = cartpos.p[2]
-    pub_msg.pose.position.y = cartpos.p[1]
+    pub_msg.pose.position.y = -cartpos.p[1]
     pub_msg.pose.position.z = cartpos.p[0]
     pub.publish(pub_msg)
 
