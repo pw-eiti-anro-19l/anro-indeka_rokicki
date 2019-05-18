3d844b5856aa7444962d52abbd07e9577707bb6e
kemot1709 <kemot1709@gmail.com>
2019-05-18 20:26:17 +0200

    First template of interpolation

--- lab4/scripts/jint.py
+++ lab4/scripts/jint.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import rospy
-import json
 import time
-from geometry_msgs.msg import PoseStamped #Publish Header header Pose pose
+import math as m
+# TODO import lab4 srv
 from sensor_msgs.msg import JointState #Subscribe Header header string[] name float64[] position float64[] velocity float64[] effort
 #Pose Point position Quaternion orientation
 #Point float64 x float64 y float64 z
@@ -13,31 +13,47 @@ def my_node():
     rospy.init_node('JINT', anonymous=True)
     global pub
     pub = rospy.Publisher('pub_joint', JointState, queue_size=10)
-    #rospy.Subscriber('jint_control', JointState, callback)
+    rospy.Subscriber('service', service, callback)
     rospy.spin()
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + 'I heard position %f from %s', data.position[0], data.name[0])
 
-    #rate = rospy.Rate(10) # 10hz
-    #while()
+    #dpos = {data.position[0]-m.pos[0], data.position[1]-m.pos[1], data.position[2]-m.pos[2]} 
+    #lenght = m.sqrt(m.pow(dpos[0],2)+m.sqrt(m.pow(dpos[1],2)+m.pow(dpos[2],2)))
+    #if (lenght/data.position[3]>TOP_SPEED)
+	#rospy.loginfo("Too fast")
+	#return???????
+    #i = data.position[3]/RATE
+
+    # interpolacja
+    #dpos = dpos/i
+
+    #rate = rospy.Rate(RATE)
     pub_msg = JointState
     pub_msg.header.frame_id = "base_link"
+    #while(i) 
+        #pub_msg.position[0] = pos[0] + dpos[0]
+        #pub_msg.position[1] = pos[1] + dpos[1]
+        #pub_msg.position[2] = pos[2] + dpos[2]
+        #pub.publish(pub_msg)
+	#i--
+    #rate.sleep
+
+    # wyeliminowanie niedokładności
+    #pos[0] = data.position[0]
+    #pos[1] = data.position[1]
+    #pos[2] = data.position[2] 
     pub_msg.position[0] = data.position[0]
     pub_msg.position[1] = data.position[1]
     pub_msg.position[2] = data.position[2]
     pub.publish(pub_msg)
-    #rate.sleep
+
 
 if __name__ == '__main__':
-    with open(os.path.dirname(os.path.realpath(__file__)) + '/../dh.json', 'r') as file:
-	params = json.loads(file.read())
-	a1, d1, al1, th1 = params['i1']
-	a2, d2, al2, th2 = params['i2']
-	a3, d3, al3, th3 = params['i3']
-	a1, d1, al1, th1 = float(a1), float(d1), float(al1), float(th1)
-	a2, d2, al2, th2 = float(a2), float(d2), float(al2), float(th2)
-	a3, d3, al3, th3 = float(a3), float(d3), float(al3), float(th3)
+#define    RATE = 10
+#define    TOP_SPEED = 1
+    #pos = {0, 0, 0}
     try:
         my_node()
     except rospy.ROSInterruptException:
